0579c62375f75c292f333f5aeef9cb9e79649cbc
VitorHugoCD <vitordelantonia@gmail.com>
2024-06-11 20:33:54 -0300

    feat[major]: cadastro de mangas / fix[minor]: erro do uryel já funfa

--- VIEW/mangas.php
+++ VIEW/mangas.php
@@ -7,7 +7,7 @@
     $servicoManga = new \BUSINESS\MangaService();
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
